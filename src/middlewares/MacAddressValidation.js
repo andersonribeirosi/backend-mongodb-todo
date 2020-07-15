1910e14ba53606b9a8b340065756a870ae59bb93
Anderson <andersonribeiro.si@hotmail.com>
2020-07-14 22:46:13 -0300

    Implementado o endpoint de filtro por macaddress da tarefa e validação do filtro

---
+++ src/middlewares/MacAddressValidation.js
@@ -0,0 +1,9 @@
+const MacAddress = (req, res, next) => {
+  if (!req.body.macaddress) {
+    return res.status(400).json({ error: 'macaddress é obrigatório' });
+  } else {
+    next();
+  }
+};
+
+module.exports = MacAddress;
